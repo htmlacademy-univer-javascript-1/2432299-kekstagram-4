b20e27e767c68525ec2d374120c62d7e2de13ba0
Kirill <alex.ujikol@mail.ru>
2023-10-06 17:10:00 +0500

    module 2 task

---
+++ js/functions.js
@@ -0,0 +1,19 @@
+const checkLength = (string, strLength) => string.length <= strLength;
+
+const checkPalidromes = (string) => {
+  string = string.replaceAll(' ', '').toLowerCase();
+  return string === string.split('').reverse().join('');
+}
+
+const getNums = function (input) {
+  let result = '';
+  input = String(input).replaceAll(' ', '');
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] == Number(input[i])) {
+      result += input[i];
+    }
+  }
+
+  return parseInt(result, 10);
+}
