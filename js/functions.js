1302953396af47c038d1ba2172d720fe0c48ed80
Kirill <alex.ujikol@mail.ru>
2023-10-27 17:53:43 +0500

    module5-task2

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,24 @@
-const checkStringLength = (string, strLength) => string.length <= strLength;
+const getTimeInMins = (time) => {
+  const timeSplit = time.split(':').map(Number);
+
+  return timeSplit[0] * 60 + timeSplit[1];
+}
+
+const checkIfInTime = (dayStartTime, dayEndTime, meetingStartTime, meetingDuration) => {
+  let dayStartInMins = getTimeInMins(dayStartTime);
+  let dayEndInMins = getTimeInMins(dayEndTime);
+  let meetingStartInMins = getTimeInMins(meetingStartTime);
+
+  return (meetingStartInMins >= dayStartInMins) && (meetingStartInMins + meetingDuration <= dayEndInMins);
+}
+
+checkIfInTime('08:00', '17:30', '14:00', 90);
+checkIfInTime('8:0', '10:0', '8:0', 120);
+checkIfInTime('08:00', '14:30', '14:00', 90);
+checkIfInTime('14:00', '17:30', '08:0', 90);
+checkIfInTime('8:00', '17:30', '08:00', 900);
+
+/*const checkStringLength = (string, strLength) => string.length <= strLength;
 
 const checkPalidromes = (string) => {
   const reversedString = string.replaceAll(' ', '').toLowerCase();
@@ -20,4 +40,4 @@ const getNums = function (input) {
 
 checkStringLength('проверяемая строка', 20);
 checkPalidromes('Лёша на полке клопа нашёл ');
-getNums('2023 год');
+getNums('2023 год');*/
